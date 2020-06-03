a35e8674080eac42b2e7fe5aeab2048c8f0d7360
xiaohupei <mail2xiaohupei@163.com>
2020-06-03 09:56:46 +0800

    refine vendor page

--- src/views/Vendor.vue
+++ src/views/Vendor.vue
@@ -341,29 +341,27 @@ export default {
   created() {
     let storedToken = sessionStorage.getItem('token');
     if ((storedToken != undefined || storedToken != null) && storedToken != 'logout') {
-		        this.$api.defaults.headers.common['X-Auth-Token'] = storedToken
+		  this.$api.defaults.headers.common['X-Auth-Token'] = storedToken
     // if (storedToken != undefined || storedToken != null) {
     //   this.$api.defaults.headers.common['X-Auth-Token'] = storedToken
     // }
 
 		    // this.$api.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxNiwiZXhwIjoxNTkzNDIxOTU5fQ.RQMa9A83CNgtObuyjJXLS8oxlx8uUjxDwmnG5InVVZg'
 
-
-    this.$api
-    let id = this.vendor.id
-      // .get("/vendor_profiles/vendor?only=true")
-      // .get("/vendor_profiles/vendor")
-      .get(`/vendor_profiles/vendor${id}`)
-
-
-      .then(response => {
-        this.vendor = response.data.vendor;
-      })
-      .catch(e => {
-        this.error.push(e);
+    // let user = sessionStorage.getItem('user');
+    // let id = this.vendor.id
+      this.$api
+        // .get("/vendor_profiles/vendor?only=true")
+        // .get("/vendor_profiles/vendor")
+        .get(`/vendor_profiles/vendor?only=true`)
+        .then(response => {
+          this.vendor = response.data.vendor;
+        })
+        .catch(e => {
+          this.error.push(e);
     });
     } else {
-		window.location.href = "https://gast.world"
+		  window.location.href = "https://gast.world"
 		}
   },
   data() {
@@ -491,7 +489,7 @@ export default {
         });
 
       this.newPromotion = {};
-      this.dialog = false;	
+      this.dialog = false;
     },
     editVendor() {
     	let updated_vendor = this.vendor;
