d21f2ec39fe99f0d9e083db6d3e77405c5ec3e03
Ellen Wendelin <ellenwendelin@Ellens-MacBook-Air.local>
2020-09-16 22:03:44 +0800

    redigned the profile page with the new cards, just have to add the apis and new translations and some JS

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,6 +5,238 @@
 			<v-list>
 				<v-list-item>
 					<v-list-item-avatar size="54" color="grey">
+						<img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" />
+						<!-- <img :src="user.wx_avatar" /> -->
+					</v-list-item-avatar>
+					<v-list-item-content>
+						<v-list-item-title class="headline">Ellen</v-list-item-title>
+						<!-- <v-list-item-title class="headline">{{
+							user.wx_nickname
+						}}</v-list-item-title> -->
+					</v-list-item-content>
+					<v-list-item-icon>
+						
+						<v-btn fab text @click="logout()"
+						>
+							<v-icon>mdi-logout</v-icon>
+						</v-btn>
+					</v-list-item-icon>
+				</v-list-item>
+			</v-list>
+		</v-layout>
+		<h3 class="mx-4 mt-3 mb-n3">{{ $t('todayOrder') }}</h3>
+		<v-layout row class="mx-auto" style="max-width: 100vw;" align-center justify-center>
+		<!-- <v-row> -->
+	      <v-col cols="12">
+	        <v-row justify="center">
+		    <v-col
+	          cols="12">
+	          <v-card
+				class="mb-2 mx-auto px-0"
+	            light>
+	            <div class="d-flex flex-no-wrap">
+	            	<v-avatar
+	                class=""
+	                size="145"
+	                height=""
+	                tile>
+	                <v-img 
+              		src="https://cdn.vuetifyjs.com/images/cards/cooking.png">
+              		</v-img>
+	              </v-avatar>
+	              <div>
+	                <v-card-title class="mt-n1 pr-0 cols-3 text-truncate" style="font-size:1.1rem;"
+	                >hello</v-card-title>
+              			<v-card-subtitle class="caption justify-center mb-n9 pr-0 cols-5 text-truncate" style="font-size:.5rem;">
+					      	<v-icon small class="align-end justify-center mr-1">mdi-clock-outline</v-icon>
+					      	32443
+						</v-card-subtitle>
+
+						<v-row
+						class="mt-3 ml-1 mb-n12 pb-n12"
+					        >
+					        <v-card-title class="body-1 deep-orange--text" style="font-size:1.1rem; font-weight: bold;">¥54</v-card-title>
+					        <v-card-title class="body-2 ml-n5 text--disabled under">¥43</v-card-title>
+					    </v-row>
+							<v-card-actions class="mx-n1 mt-7 cols-3 mb-n2">
+						      <v-btn small
+						      depressed
+						      class="white--text"
+						      color="#FFB300"
+						      @click.stop="dia = true">redeem</v-btn>
+								<v-dialog v-model="dia" max-width="290">
+									<v-card>
+										<v-layout row class="mx-auto">
+											<v-card-title class="headline">{{ $t('couponCode') }}</v-card-title>
+											<v-spacer></v-spacer>
+											<v-btn icon @click="dia = false">
+												<v-icon>mdi-close</v-icon>
+											</v-btn>
+										</v-layout>
+
+										<v-card-text class="headline">
+											6Gt647HK
+											<!-- {{ deal.token }} -->
+										</v-card-text>
+									</v-card>
+								</v-dialog>
+
+						      <v-btn
+						        depressed
+						        color="#FFB300"
+						        small
+						        outlined
+						        @click.stop="promotion = true">
+						        Details
+						      </v-btn>
+						    </v-card-actions>
+					 
+						<v-dialog v-model="promotion" width="290">
+				    		<v-layout>
+							<v-card width="290">
+							<v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" height="250px">
+							</v-img>
+								<v-layout row class="ml-3 mr-3 px-1 pt-0">
+								<v-list>
+									<v-card-title class="mb-n2 pb-0">
+										Title
+									</v-card-title>
+									<v-list-item class="mt-1 mb-n3">
+									    <v-list-item-icon class="pr-0 mr-0">
+									        <v-icon small>mdi-clock</v-icon>
+									    </v-list-item-icon>
+									    <v-list-item-title class="text-wrap font-weight-bold" style="font-size:.8rem;">34:34</v-list-item-title>
+									</v-list-item>
+										<!-- <v-list-item class="mb-0 pb-0">
+									        <v-list-item-icon class="pr-0 mr-0">
+									          <v-icon small>mdi-noodles</v-icon>
+									        </v-list-item-icon>
+
+									        <v-list-item-title class="font-weight-bold" style="font-size:.8rem;">{{ promotion.statistics.available }}{{ $t('avi') }}</v-list-item-title>
+									    </v-list-item> -->
+								
+								<v-spacer></v-spacer>
+								<v-card-text class="py-0">
+									promo desct
+								</v-card-text>
+							</v-list>
+							</v-layout>
+							<!-- <v-card-actions class="d-flex justify-center pb-3">
+									<v-btn
+										width="80%"
+										dark
+										color="#FFB300"
+										tile
+										class="buttons"
+										depressed
+										@click.stop="getCoupon(promotion)"
+									>
+										{{ $t('claim') }}
+									</v-btn>
+								</v-card-actions> -->
+								<v-layout row class="mx-5">
+									<v-list>
+										<v-list-item>
+											<v-list-item-avatar size="42" color="grey">
+												<v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" alt="Logo" />
+											</v-list-item-avatar>
+											<v-list-item-content>
+												<v-list-item-title>Vendor</v-list-item-title>
+											</v-list-item-content>
+										</v-list-item>
+										<v-list-item class="mt-1 mb-n3">
+									        <v-list-item-icon class="pr-0 mr-0">
+									          <v-icon small>mdi-home</v-icon>
+									        </v-list-item-icon>
+
+									        <v-list-item-title class="text-wrap font-weight-bold" style="font-size:.8rem;">vendor address</v-list-item-title>
+									    </v-list-item>
+										<v-list-item class="mb-0 pb-0">
+									        <v-list-item-icon class="pr-0 mr-0">
+									          <v-icon small>mdi-phone</v-icon>
+									        </v-list-item-icon>
+
+									        <v-list-item-title class="font-weight-bold" style="font-size:.8rem;">123456773</v-list-item-title>
+									    </v-list-item>
+										<v-card-text class="mb-1 mt-0 pt-0">
+											vendor description
+										</v-card-text>
+									</v-list>
+								</v-layout>
+							</v-card>
+						</v-layout>
+					</v-dialog>
+	              </div>
+	            </div>
+	          </v-card>
+	        </v-col>
+	        </v-row>
+	      </v-col>
+		</v-layout>
+		<h3 class="mx-4 mt-3">Past Orders</h3>
+		<v-layout row class="mx-auto" style="max-width: 100vw;" align-center justify-center>
+		<!-- <v-row> -->
+	      <v-col cols="12">
+	        <v-row justify="center">
+		    <v-col
+	          cols="12">
+	          <v-card
+				class="mb-2 mx-auto px-0"
+	            light>
+	            <div class="d-flex flex-no-wrap">
+	            	<v-avatar
+	                class=""
+	                size="145"
+	                height=""
+	                tile>
+	                <v-img 
+              		src="https://cdn.vuetifyjs.com/images/cards/cooking.png">
+              		</v-img>
+	              </v-avatar>
+	              <div>
+	                <v-card-title class="mt-n1 pr-0 cols-3 text-truncate" style="font-size:1.1rem;"
+	                >hello</v-card-title>
+              			<v-card-subtitle class="caption justify-center mb-n9 pr-0 cols-5 text-truncate" style="font-size:.5rem;">
+					      	<v-icon small class="align-end justify-center mr-1">mdi-clock-outline</v-icon>
+					      	32443
+						</v-card-subtitle>
+
+						<v-row
+						class="mt-3 ml-1 mb-n12 pb-n12"
+					        >
+					        <v-card-title class="body-1 deep-orange--text" style="font-size:1.1rem; font-weight: bold;">¥54</v-card-title>
+					        <v-card-title class="body-2 ml-n5 text--disabled under">¥43</v-card-title>
+					    </v-row>
+							<v-card-actions class="mx-n1 mt-7 cols-3 mb-n2">
+						      <v-btn small
+						      depressed
+						      class="white--text"
+						      color="#FFB300"
+						      disabled>redeem</v-btn>
+		
+
+						      <v-btn
+						        depressed
+						        color="#FFB300"
+						        small
+						        outlined
+						        disabled>
+						        Details
+						      </v-btn>
+						    </v-card-actions>
+					 
+						
+	              </div>
+	            </div>
+	          </v-card>
+	        </v-col>
+	        </v-row>
+	      </v-col>
+		</v-layout>
+		<!-- <v-layout column class="mx-5 mb-5">
+			<v-list>
+				<v-list-item>
+					<v-list-item-avatar size="54" color="grey">
 						<img :src="user.wx_avatar" />
 					</v-list-item-avatar>
 					<v-list-item-content>
@@ -13,7 +245,7 @@
 						}}</v-list-item-title>
 					</v-list-item-content>
 					<v-list-item-icon>
-						<!-- @@@@ FIXME - CAN I DO THIS????@@@@@--> 
+						
 						<v-btn fab text @click="logout()"
 						>
 							<v-icon>mdi-logout</v-icon>
@@ -42,7 +274,7 @@
 							{{ deal.promotion.time_slot }}
 						</v-list-item-subtitle>
 					</v-list-item-content>
-					<!-- <v-spacer></v-spacer> -->
+					
 					<v-list-item-icon @click="deal.dialog3 = true" class="my-0">
 						<v-btn fab text>
 							<v-icon>mdi-key</v-icon>
@@ -115,7 +347,7 @@
 			>
 				{{ $t('browseDeals') }}
 			</v-btn>
-		</v-layout>
+		</v-layout> -->
 	</div>
 </template>
 <script>
@@ -159,7 +391,9 @@
 				customerID: 1,
 				deals: [],
 				res: [],
+				promotion: false,
 				snackbar: false,
+				dia: false,
 				//what is the data we get from the api? to put the username and avatar?
 				user: JSON.parse(sessionStorage.getItem('user'))
 			};
